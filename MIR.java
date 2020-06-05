4d5fa982fbbdb3c6abbb0c0613ad1c6e5c4ae29d
Kiryl Shvachka <kiryl92@gmail.com>
2020-06-06 01:22:49 +0200

    update MIR

--- MIR.java
+++ MIR.java
@@ -1,9 +1,16 @@
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 
 public class MIR {
     public static void main(String[] args) {
+        String objref = "43 4f 4e 47 52 41 54 53 2c 20 4e 4f 57 20 55 52 20 47 41 59";
         Scanner sc = new Scanner(System.in);
+        StringTokenizer st = new StringTokenizer(objref);
+        StringBuilder output = new StringBuilder();
+
+        for(int i = 0; i < 20; i++) 
+            output.append((char) Integer.parseInt(st.nextToken(" "), 16));
 
         String[] MIR = {
             "ADDR(0xHH hex)",
@@ -35,12 +42,26 @@ public class MIR {
             System.out.println("Insert: " + MIR[i]);
             String n = sc.nextLine();
         }
+
+        sc.close();
+
         System.out.print("Processing your request");
-        for(int i = 0; i < 900000000; i++) {
-            if(i % 300000000 == 0)
+
+            for(int i = 0; i < 5; i++) {
                 System.out.print(" . ");
+                try {
+                    Thread.sleep(2000);
+                }
+                catch (Exception e) {
+                System.err.println("boh");
+            }
+            
         }
-        System.out.println();
-        System.out.println("CONGRATS, NOW UR GAY");
+        // System.out.println("\n\n############ OUTPUT ############\n");
+        // System.out.println(">>> " + output + " <<<\n");
+        // System.out.println("################################");
+
+        Out.show(output.toString());
+
     }
 }
